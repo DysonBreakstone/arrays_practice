f396eef40a3d7c3ea0dd006dd5577c309274c01e
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-01-04 12:04:24 -0700

    Initial commit

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+colors = ["Yellow", "Green", "Blue", "Greener", "Orange"]
+numbers = [34, 35 ,36, 37, 99430]
+averages = [55.4, 13.34, 99.5, 87.26, 99.034]
+puppy_iscute = [false, true, true, false, true]
