81c6c0634adf3f079dfba5903d7dd8a7ce9fcca6
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-01-04 12:25:03 -0700

    Add array methods with explanation

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,25 @@ colors = ["Yellow", "Green", "Blue", "Greener", "Orange"]
 numbers = [34, 35 ,36, 37, 99430]
 averages = [55.4, 13.34, 99.5, 87.26, 99.034]
 puppy_iscute = [false, true, true, false, true]
+
+# This will delete the last element in the array
+colors.pop
+
+#This will add an element on the end of the array
+colors.append("Purple","Glellow","Blorange")
+#So will this. I'm not sure about the difference.
+colors.push("Gold","Silver")
+
+#This will remove the first element in the array
+numbers.shift
+
+#This will place a new item at element 0 in the array
+averages.unshift (45.78)
+
+#This will state the length of the array
+puts puppy_iscute.length
+
+puts colors
+puts numbers
+puts averages
+puts puppy_iscute
